0023a2458a7f58cf97ba7d20f5851acca837c058
Okropovich <89385037063k@gmail.com>
2026-05-20 23:51:51 +0300

    ok2

--- src/main/java/com/school/models/Student.java
+++ src/main/java/com/school/models/Student.java
@@ -11,7 +11,7 @@ public class Student {
     private String name;
     private int age;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "faculty_id")
     @JsonIgnore
     private Faculty faculty;
